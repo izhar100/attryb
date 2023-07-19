4bce4ac3f77ff8c9a3a4aa3ef1cb781aa86c9655
izhar100 <ezharashraf523@gmail.com>
2023-07-19 12:57:42 +0530

    backend almost done

--- attryb/backend/index.js
+++ attryb/backend/index.js
@@ -3,8 +3,14 @@ const app=express()
 app.use(express.json())
 const cors=require("cors")
 const { connection } = require("./config/db")
+const { userRouter } = require("./routes/user.router")
+const { oemRouter } = require("./routes/oemSpecs.router")
+const { inventoryRouter } = require("./routes/inventory.route")
 app.use(cors())
 require("dotenv").config()
+app.use("/user",userRouter)
+app.use("/spec",oemRouter)
+app.use("/inventory",inventoryRouter)
 
 app.listen(process.env.port,async()=>{
     try {
